50bb6860c3ea6a3f433f3cafdd2267ea9a0b6337
Amon Rhaniery <amonrhaniery27@gmail.com>
2021-08-11 12:08:49 -0300

    fix arrows

--- src/components/carousel/bootstrapCarousel.ts
+++ src/components/carousel/bootstrapCarousel.ts
@@ -1,10 +1,19 @@
 import { Carousel } from "react-bootstrap";
 import styled from "styled-components";
+import { BLACK } from "../../styles/constants";
 
 export const BootstrapCarousel = styled(Carousel)`
   height: 100%;
-
+  width: 110vh;
   .carousel-inner {
     height: 100%;
+  }
+  .carousel-control-next-icon {
+    background-color: ${BLACK};
+    border-radius: 10px;
+  }
+  .carousel-control-prev-icon {
+    background-color: ${BLACK};
+    border-radius: 10px;
   }
 `;
--- src/components/container/carousel/carouselContainer.ts
+++ src/components/container/carousel/carouselContainer.ts
@@ -4,6 +4,7 @@ export const CarouselContainer = styled.div`
   height: 68vh;
   background-image: url("background.png");
   background-size: cover;
-  /*   display: flex;
-  align-items: center; */
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
